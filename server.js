cc0ad894f12b0f08855f286bf8ec67fd112f8b49
HR HR&FINANCE FINANCE <hr-finance@vcidex.com>
2026-02-14 15:44:21 +0530

    Updated port for deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const boyfriendName = "My Love ❤️";
 const anniversaryDate = new Date("2024-06-15");
